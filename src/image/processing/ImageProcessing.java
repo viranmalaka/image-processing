6ae05746b2004f471bb91590b1bd1f4302cfbddf
viranmalaka <malaka.14@cse.mrt.ac.lk>
2017-05-18 18:23:21 +0530

    load sample image

--- src/image/processing/ImageProcessing.java
+++ src/image/processing/ImageProcessing.java
@@ -5,17 +5,45 @@
  */
 package image.processing;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import views.MainFrame;
+
 /**
  *
  * @author malaka
  */
 public class ImageProcessing {
 
+    private static BufferedImage image;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        String path = "1.png";
+        try {
+            image = ImageIO.read(new File(path));
+            new MainFrame().setVisible(true);
+            
+//            new frmOptions().setVisible(true);
+        } catch (IOException ex) {
+            Logger.getLogger(ImageProcessing.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+
+    public static BufferedImage getImage() {
+        return image;
+    }
+
+    public static void setImage(BufferedImage image) {
+        ImageProcessing.image = image;
     }
     
 }
